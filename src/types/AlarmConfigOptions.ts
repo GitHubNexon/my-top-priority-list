ab157921a68f118f018247199e50639163c83555
FindingShrek <fabellajoshua.m21@gmail.com>
2025-09-24 15:12:39 +0800

    chore(rn): Remove deprecated alarm service files

--- src/types/AlarmConfigOptions.ts
+++
@@ -1,9 +0,0 @@
-export interface RingtoneInfo {
-  title: string;
-  uri: string;
-}
-
-export interface AlarmConfigOptions {
-  snoozeMinutes: number;
-  selectedRingtone: RingtoneInfo | null; // currently selected ringtone
-}
