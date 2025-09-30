733cabd3393523b959ac5ae43c4e51e4d6cf7688
FindingShrek <fabellajoshua.m21@gmail.com>
2025-09-30 15:25:06 +0800

    feat(android): Refactor boot alarm restoration and headless JS task

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { name as appName } from './app.json';
+import './src/services/BootRestoreTask';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { enableScreens, enableFreeze } from 'react-native-screens';
