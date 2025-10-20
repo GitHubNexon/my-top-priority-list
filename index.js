de750e32f65f0dc3461f899918a25821cd138073
FindingShrek <fabellajoshua.m21@gmail.com>
2025-10-20 23:31:40 +0800

    Fix: Resolve React Native Firebase deprecation warnings

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ import './src/services/BootRestoreTask';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { enableScreens, enableFreeze } from 'react-native-screens';
+import { LogBox } from 'react-native';
 // If you have background tasks (like notifications, messaging):
 // import './src/services/backgroundHandler';
+
+LogBox.ignoreLogs([
+    // React Native Firebase v22 deprecation warnings
+    'This method is deprecated (as well as all React Native Firebase namespaced API)',
+    'Please see migration guide for more details: https://rnfirebase.io/migrating-to-v22',
+]);
 enableScreens(true);
 enableFreeze(true);
 AppRegistry.registerComponent(appName, () => App);
